1617ed6e34d7f8be1ef1871fdbba8eb466a725b7
Niels Heirbaut <niels@opilo.nl>
2024-12-11 09:59:59 +0000

    Implement calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+  return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let total = 0;
+  array.forEach(n => total += n);
+
+  return total;
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  let total = 1;
+  array.forEach(n => total *= n);
 
+  return total;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+  return x ** y;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  let product = 1;
+  for (let i = n; i > 0; i--) {
+    product *= i;
+  }
+
+  return product;
 };
 
 // Do not edit below this line
