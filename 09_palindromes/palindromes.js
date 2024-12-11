6344217bd39cbd1800df02127ab7fd1e3b57f72f
Niels Heirbaut <niels@opilo.nl>
2024-12-11 13:40:45 +0000

    Palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    const allowed = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+    const cleanedWord = word
+        .toLowerCase()
+        .split('')
+        .filter(c => allowed.includes(c))
+        .join('');
+
+    const reversedWord = cleanedWord
+        .split('')
+        .reverse()
+        .join('');
+
+    return cleanedWord === reversedWord;
 };
 
 // Do not edit below this line
