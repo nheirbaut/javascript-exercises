8bf136a0d4fd2879ec046653400d45e17a06db27
Niels Heirbaut <niels@opilo.nl>
2024-12-11 14:19:24 +0000

    Solution for find the oldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const getAge = function(person) {
+        if (person.yearOfDeath == undefined) {
+            return (new Date()).getFullYear() - person.yearOfBirth;
+        }
 
+        return person.yearOfDeath - person.yearOfBirth;
+    }
+
+    const sortedByAge = people
+        .sort((person1, person2) => {
+            const age1 = getAge(person1);
+            const age2 = getAge(person2);
+
+            return age1 > age2 ? -1 : 1;
+        });
+
+    return sortedByAge[0];
 };
 
 // Do not edit below this line
